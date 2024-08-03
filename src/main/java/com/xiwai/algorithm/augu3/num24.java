bbd290cf7db71d1c3323f5a6a12d7641a2d88486
cyxiwai <A15618827017@gmail.com>
2024-08-03 12:13:21 +0800

    八月三号，今天的心得是对指针用法有了更深入的了解，实际上，xxx.next真的是非常秒的用法，它既是一个节点，又可以标示当前节点的指向，本质是地址。而对temp=temp.next有了非常新的理解，实际上dummyNode之所以能保证进行过调整后还能链接头节点，就是第一次temp=temp.next来做的，temp和dummy是一个地址，所以两个节点实际上相同，做了第一次以后，直接把后继节点的地址赋值给了temp，那么temp就和虚拟头dummy无关了，而dummy就记录了链表的信息，作为链表的头节点。

---
+++ src/main/java/com/xiwai/algorithm/augu3/num24.java
@@ -0,0 +1,41 @@
+package com.xiwai.algorithm.augu3;
+
+public class num24 {
+
+}
+
+class Solution24{
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            ListNode dummyNode = new ListNode();
+            dummyNode.next = head;
+            ListNode temp=dummyNode;
+            ListNode firstNode =new ListNode();
+            ListNode secondNode =new ListNode();
+            while (temp.next!=null&&temp.next.next != null) {
+                firstNode=temp.next;
+                secondNode = temp.next.next;
+                firstNode.next=secondNode.next;
+                secondNode.next=firstNode;
+                temp.next=secondNode;
+                temp=firstNode;
+            }
+            return dummyNode.next;
+        }
+    }
+
+
+
+}
+
+
