8e8b02515411d2232336bb9f1b2df857e98e3975
cyxiwai <A15618827017@gmail.com>
2024-08-13 18:17:29 +0800

    还在还债

--- src/main/java/com/xiwai/algorithm/augu13/num429.java
+++ src/main/java/com/xiwai/algorithm/augu13/num429.java
@@ -1,29 +1,57 @@
-package com.xiwai.algorithm.augu13;
-
-import java.util.List;
-
-public class num429 {
-
-}
-
-
-class Solution {
-    public List<List<Integer>> levelOrder(Node root) {
-
-    }
-}
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
+//package com.xiwai.algorithm.augu13;
+//
+//import java.util.ArrayList;
+//import java.util.Deque;
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//public class num429 {
+//
+//}
+//
+//
+//class Solution429 {
+//    public List<List<Integer>> levelOrder(Node root) {
+//        Deque<Node> deque = new LinkedList<>();
+//        List<List<Integer>> resList = new ArrayList<>();
+//        Node temp = new Node();
+//        int lenth = 0;
+//        if (root == null) {
+//            return resList;
+//        }
+//        deque.offer(root);
+//        while (!deque.isEmpty()) {
+//            lenth = deque.size();
+//            List<Integer> listTemp = new ArrayList<>();
+//            for (int i = 0; i < lenth; i++) {
+//                temp = deque.remove();
+//                listTemp.add(temp.val);
+//                if (temp.children != null && !temp.children.isEmpty()) {
+//
+//                    for (Node node : temp.children) {
+//                        deque.offer(node);
+//                    }
+//                }
+//            }
+//            resList.add(listTemp);
+//        }
+//        return resList;
+//    }
+//}
+//
+//class Node {
+//    public int val;
+//    public List<Node> children;
+//
+//    public Node() {
+//    }
+//
+//    public Node(int _val) {
+//        val = _val;
+//    }
+//
+//    public Node(int _val, List<Node> _children) {
+//        val = _val;
+//        children = _children;
+//    }
+//}
---
+++ src/main/java/com/xiwai/algorithm/augu13/num515.java
@@ -0,0 +1,56 @@
+package com.xiwai.algorithm.augu13;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class num515 {
+    public static void main(String[] args) {
+        Solution515 solution515 = new Solution515();
+        TreeNode treeNode7 = new TreeNode(7);
+        TreeNode treeNode6 = new TreeNode(6);
+        TreeNode treeNode5 = new TreeNode(5);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode3 = new TreeNode(3, treeNode6, treeNode7);
+        TreeNode treeNode2 = new TreeNode(2, treeNode4, treeNode5);
+        TreeNode treeNode1 = new TreeNode(1, treeNode2, treeNode3);
+        List<Integer> res = solution515.largestValues(treeNode1);
+        for (Integer i : res) {
+            System.out.println(i);
+        }
+    }
+}
+
+
+class Solution515 {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> resList = new ArrayList<>();
+        Deque<TreeNode> deque = new LinkedList<>();
+        if (root == null) {
+            return resList;
+        }
+        TreeNode temp = root;
+        deque.offer(temp);
+        int lenth = 0;
+        while (!deque.isEmpty()) {
+            lenth = deque.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < lenth; i++) {
+                temp = deque.remove();
+                max = Math.max(temp.val, max);
+                if (temp.left != null) {
+                    deque.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    deque.offer(temp.right);
+                }
+            }
+            resList.add(max);
+        }
+
+
+        return resList;
+
+    }
+}
