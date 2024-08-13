6053401ad83652002dcd214d09d10d6d2c64a51d
cyxiwai <A15618827017@gmail.com>
2024-08-13 16:10:55 +0800

    继续还

---
+++ src/main/java/com/xiwai/algorithm/augu13/num107.java
@@ -0,0 +1,40 @@
+package com.xiwai.algorithm.augu13;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class num107 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution107 {
+    public List<List<Integer>> resList = new ArrayList<>();
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        int deep=0;
+        DiGui107(root,deep);
+        Collections.reverse(resList);
+        return resList;
+    }
+    public void DiGui107(TreeNode root,int deep)
+    {
+        if(root==null)
+        {
+            return ;
+        }
+        deep++;
+        if(resList.size()<deep)
+        {
+            List<Integer> temp = new ArrayList<>();
+            resList.add(temp);
+        }
+        resList.get(deep-1).add(root.val);
+        DiGui107(root.left,deep);
+        DiGui107(root.right,deep);
+    }
+
+}
---
+++ src/main/java/com/xiwai/algorithm/augu13/num429.java
@@ -0,0 +1,29 @@
+package com.xiwai.algorithm.augu13;
+
+import java.util.List;
+
+public class num429 {
+
+}
+
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+
+    }
+}
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
